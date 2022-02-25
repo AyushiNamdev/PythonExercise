28a480913ba13c69a591e01e611b893067ea96b6
AyushiNamdev <ayushinamdev2@gmail.com>
2022-02-25 12:50:44 -0500

    if exercise

---
+++ ex29.py
@@ -0,0 +1,23 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print ("too many cats! the world is doomed.")
+if people > cats:
+    print("not many cats! the world is saved")
+if people <dogs:
+    print("good")
+if people > dogs:
+    print("bad")
+
+dogs +=5
+
+if people >= dogs:
+    print("people are greater than or equal to dogs")
+
+if people <= dogs:
+    print("people are less than or euqal to dogs")
+
+if people == dogs:
+    print("people are dogs")
