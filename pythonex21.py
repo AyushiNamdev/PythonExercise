9244625fb375e999c51d17f775d360c75c449ea2
AyushiNamdev <ayushinamdev2@gmail.com>
2022-02-25 12:52:24 -0500

    exercise 21

---
+++ pythonex21.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    print("adding %d + %d" %(a, b))
+    return a + b
+
+def subtract(a, b):
+    print("subtracting %d - %d" %(a, b))
+    return  a - b
+
+def multiply(a,b):
+    print("multiplying %d * %d" %(a,b))
+    return a*b
+
+print(" lets do some math")
+
+age = add(12, 45)
+height= subtract(34, 12)
+weight = multiply(23, 54)
+
+print("Age: %d \nheight: %d \nweight: %d" %(age, height, weight))
+
+
